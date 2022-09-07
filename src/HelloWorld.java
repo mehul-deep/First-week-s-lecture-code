a930af5e4e918989b6ae42d4abb4e715a481998c
Mehul_Deep <mehul.ddep@jacks.sdstate.edu>
2022-09-07 11:19:48 -0500

    Have edited and done along with the first weeks video

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class HelloWorld {
     public static void main(String[] args){
+        In fileInput = new In(args[0]); //This is to open the file
+        while(!fileInput.isEmpty()) {
+            String data = fileInput.readString(); //Used to reading from tthe file
+            StdOut.println(data); //This is to print the result.
+        }
 
     }
 }
